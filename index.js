56118ca5565f265b0287c22e35a77aae1432a5fc
Andreas Ohlsson <andreas.ohlsson@applio.se>
2024-12-23 11:48:11 +0100

    Fix port setting for Deploy

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // server.mjs
 import { createServer } from 'node:http';
 
+const port = process.env.PORT || 3000;
+
 const server = createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World!!!\n');
 });
 
 // starts a simple http server locally on port 3000
-server.listen(3000, '127.0.0.1', () => {
+server.listen(port, '127.0.0.1', () => {
   console.log('Listening on 127.0.0.1:3000');
 });
 
